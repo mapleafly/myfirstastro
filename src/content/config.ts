07ee55e4d2c5741a94e1ff2ce2288d9ba0539994
leoxxx <lifxue@gmail.com>
2024-01-26 12:36:38 +0800

    使用内容集合

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
-import { defineCollection, z } from 'astro:content';
-
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+// 从 `astro:content` 导入辅助工具
+import { z, defineCollection } from "astro:content";
+// 为每一个集合定义一个 `type` 和 `schema`
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
 });
-
-export const collections = { blog };
+// 导出一个单独的 `collections` 对象来注册你的集合
+export const collections = {
+  posts: postsCollection,
+};
